e8faee39b1930aad5eee601f6e9abf3625045d45
oezeb <eouedraogo4@gmail.com>
2023-08-25 03:40:58 +0800

    add privacy policy and google-services

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.gms.google-services")
 }
 
 android {
@@ -37,4 +38,6 @@ dependencies {
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("com.google.android.material:material:1.9.0")
     implementation("com.google.android.gms:play-services-ads:22.2.0")
+    implementation(platform("com.google.firebase:firebase-bom:32.2.2"))
+    implementation("com.google.firebase:firebase-analytics-ktx")
 }
