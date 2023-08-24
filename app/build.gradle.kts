9c6d617e3b9b0de3c09d60b51f94d7c72492b614
oezeb <eouedraogo4@gmail.com>
2023-08-25 04:26:00 +0800

    edit admob ids

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.github.oezeb.currencyconverter"
         minSdk = 24
         targetSdk = 33
-        versionCode = 3
-        versionName = "1.0.2"
+        versionCode = 4
+        versionName = "1.0.3"
     }
 
     buildTypes {
